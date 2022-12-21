72cf726fec18231c065a460717ed74f19bb05598
IslamelGebaly <islamlang@gmail.com>
2022-12-22 00:02:55 +0200

    button

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,42 @@
+isOn = false;
+
 function app(){
     const doc = document;
+
+    //Setting up a container for the page
     const container = doc.createElement("div");
-    container.setAttribute("style",
-        `position: 
-        relative; 
-        left:450px;
-        top: 200px; 
-        border-style:double; 
+    container.setAttribute("style", 
+        `width: 866px;
+        height: 700px;
+        margin: 0px 0px 0px 0px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center`
+    );
+
+    //timer display
+    const display = doc.createElement("div");
+    display.setAttribute("style",
+        `border-style:double; 
         border-width:10px; 
         text-align:center;
         color: white;
         background-color:blue;
         width:400px`
     )
-    doc.body.appendChild(container);
-
+    container.appendChild(display);
+    
+    ///timer element
     const timer = doc.createElement("h1");
     timer.textContent = "00:00:00";
-    container.appendChild(timer);
+    display.appendChild(timer);
 
+    //button
     button = doc.createElement("button");
     button.textContent = "Start";
     button.setAttribute("style", 
-        `position: relative;
-        top:225px; left:775px;
-        border-style:double;
+        `border-style:double;
         border-width:5px;
         border-color: white;
         text-align: center;
@@ -32,14 +44,29 @@ function app(){
         color: white;
         background-color: green;
         width: 100px;
-        height: 50px`
-    )
+        height: 50px;
+        position: relative;
+        margin-left: 320px`
+    );
 
-    doc.body.appendChild(button);
+    button.addEventListener('click', e => {
+        e.textContent = "Stop";
+        e.setAttribute("style", 
+            `border-style:double;
+            border-width:5px;
+            border-color: white;
+            text-align: center;
+            font-size: 20px;
+            color: white;
+            background-color: red;
+            width: 100px;
+            height: 50px;
+            position: relative;
+            margin-left: 320px`);
+    });
+    container.appendChild(button);
+    doc.body.appendChild(container);
 }
 
-function updateStopWatch(){
-
-}
 
 app()
